ed5f947260c8f5f3ba0ac5757602f2c7965b40f0
Justin Moore <justin.moore@rhsps.com>
2019-04-09 23:26:01 -0400

    Fixed the universe hole

--- ch3-ray-tracer/src/main.rs
+++ ch3-ray-tracer/src/main.rs
@@ -11,7 +11,7 @@ fn unit_vector(v: Vector3<f32>) -> Vector3<f32> {
 }
 
 fn vector_length(v: Vector3<f32>) -> f32 {
-    let length_squared: f32 = v.x*v.x + v.y*v.y + v.z+v.z;
+    let length_squared: f32 = v.x*v.x + v.y*v.y + v.z*v.z;
     length_squared.sqrt()
 }
 
@@ -35,12 +35,27 @@ fn main() -> std::io::Result<()> {
         while i < nx {
             let u: f32 = i / nx;
             let v: f32 = j / ny;
-            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
+            let uh: Vector3<f32> = u*horizontal;
+            let vv: Vector3<f32> = v*vertical;
+            let r = Ray::new(origin, lower_left_corner + uh + vv);
+            let mut shadow_r = Ray::new(origin, lower_left_corner + uh + vv);
             let col = color(r);
             let ir: f32 = 255.99 * col.x;
             let ig: f32 = 255.99 * col.y;
             let ib: f32 = 255.99 * col.z;
             
+                
+            if i == 10.0 && j == 20.0 {
+                println!("Value of u: {}", u);
+                println!("Value of v: {}", v);
+                println!("Value of u*horizontal x: {}, y: {}", uh.x, uh.y);
+                println!("Value of v*vertical x: {}, y: {}", vv.x, vv.y);
+                let x = lower_left_corner + uh + vv;
+                println!("Value of ray destination x: {}, y: {}", x.x, x.y);
+                let unit_direction: Vector3<f32> = unit_vector(shadow_r.direction());
+                println!("Value of Unit Vector x: {}, y: {}", unit_direction.x, unit_direction.y);
+            }
+
             file_contents = file_contents + &format!("{} {} {}\n", ir, ig, ib);
             i = i + 1.0f32;
         }
