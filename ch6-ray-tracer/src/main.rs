a9a8dc3adee5fdd77a1796c5ba164d5063309a8a
Justin Moore <justin.moore@rhsps.com>
2019-05-18 12:53:34 -0400

    Checking in work so far; generating black screens

--- ch6-ray-tracer/src/main.rs
+++ ch6-ray-tracer/src/main.rs
@@ -1,11 +1,15 @@
 extern crate raytracing;
 extern crate cgmath;
+extern crate rand;
 
-use raytracing::*;
-use raytracing::actors::Sphere;
 use std::fs::File;
 use std::io::prelude::*;
 use cgmath::Vector3;
+use rand::prelude::*;
+
+use raytracing::*;
+use raytracing::actors::Sphere;
+use raytracing::camera::Camera;
 
 fn unit_vector(v: Vector3<f32>) -> Vector3<f32> {
     v / vector_length(v)
@@ -46,26 +50,35 @@ fn color(mut r: Ray, world: &[&Hittable]) -> Vector3<f32> {
 fn main() -> std::io::Result<()> {
     let nx: u8 = 200;
     let ny: u8 = 100;
-    let mut j: u8 = ny - 1;
-    let mut file_contents = format!("P3\n{} {}\n255\n", nx, ny);
-    let lower_left_corner = Vector3::new(-2.0f32, -1.0f32, -1.0f32);
-    let horizontal = Vector3::new(4.0f32, 0.0f32, 0.0f32);
-    let vertical = Vector3::new(0.0f32, 2.0f32, 0.0f32);
-    let origin = Vector3::new(0.0f32, 0.0f32, 0.0f32);
+    let ns: u8 = 100;
+    let mut s: u8 = 0;
 
+    let mut file_contents = format!("P3\n{} {}\n255\n", nx, ny);
+    
     let s1 = Sphere::new(Vector3::new(0f32,0f32,-1f32), 0.5f32);
     let s2 = Sphere::new(Vector3::new(0f32,-100.5f32,-1f32), 100f32);
     let world: Vec<&Hittable> = vec![&s1, &s2];
+    let cam = Camera::new();
 
+    let mut j: u8 = ny - 1;
     loop {
         let mut i: u8 = 0;
         while i < nx {
-            let u: f32 = i as f32 / nx as f32;
-            let v: f32 = j as f32 / ny as f32;
-            let mut r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
-
-            let p = r.point_at_parameter(2.0);
-            let col = color(r, &world);
+            let mut col = Vector3::new(0f32,0f32,0f32);            
+            while s < ns {
+                let rand1: f32 = rand::random();
+                let rand2: f32 = rand::random();
+                
+                let u: f32 = (rand1 * i as f32) / nx as f32;
+                let v: f32 = (rand2 * j as f32) / ny as f32;
+                let mut r = cam.get_ray(u, v);
+                let _p = r.point_at_parameter(2.0);
+                col = col + color(r, &world);
+                s = s + 1;
+                println!("{}, {}, {}", col.x, col.y, col.z); // has values
+            }
+            println!("{}, {}, {}", col.x, col.y, col.z); // No values
+            col = col / ns as f32;
             let ir = (255.99 * col.x) as u8;
             let ig = (255.99 * col.y) as u8;
             let ib = (255.99 * col.z) as u8;
